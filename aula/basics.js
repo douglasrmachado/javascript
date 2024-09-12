0d77c8b922d0f93b95c73deb2d850e4de45dacb7
douglasrmachado <douglasroqmachado@gmail.com>
2024-09-12 08:24:11 -0300

    first commit

---
+++ aula/basics.js
@@ -0,0 +1,49 @@
+ // number 
+ const x = 10;
+
+ // number
+ const y = 2.5;
+
+ // string
+ const nome = "Leandro";
+
+ // string
+ const sobrenome = 'Clayton';
+
+ // boolean
+ const verdadeiro = true;
+ const falso = false;
+
+ // null
+ const nulo = null;
+
+ // undefined
+ const nao_definido = undefined;
+
+ console.log(nome, sobrenome);
+ console.log(x, y);
+
+
+ console.log(verdadeiro);
+ console.log(falso);
+
+ console.log(nulo);
+
+ console.log(nao_definido);
+
+ // Comentário 
+ /* Comentário */
+
+ // typeof = shows the data type
+ console.log(typeof x);
+ console.log(typeof verdadeiro);
+
+ // let = allows changing the variable value
+ let nomes = "Josué, Vinicius"; 
+ nomes = "Asafh, Jacke";
+
+ console.log(nomes);
+
+ // null x undefined
+ // undefined = I create a variable without a value
+ // null = programmer creates a variable with a "variable = null"
---
+++ aula/operators.js
@@ -0,0 +1,50 @@
+const x = 4;
+const y = 5;
+
+const soma = x + y;
+const subtracao = x - y;
+const multiplicacao = x * y;
+const divisao = x / y;
+const restoDeDivisao = y % x;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+
+// shows the division rest
+console.log(3 % 2);
+console.log(5 % 2);
+console.log(11 % 3);
+
+// incrementation
+let z = 1;
+z = z + 1;
+z += 1;
+z++;
+
+console.log(z);
+
+// decrementation
+let a = 10;
+a = a - 1;
+a -= 1;
+a--;
+console.log(a);
+
+a += 50;
+console.log(a);
+z -= 100;
+console.log(z);
+
+let c = 10;
+c *= 2;
+console.log(c);
+
+let d = 90;
+d /= 3;
+console.log(d);
+
+let e = 9;
+e %= 2;
+console.log(e);
