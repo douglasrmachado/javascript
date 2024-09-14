a8f6e3788a3438357b6d1892994b7ab739371e0c
douglasrmachado <douglasroqmachado@gmail.com>
2024-09-14 12:13:45 -0300

    functions

--- functions/functions.js
+++ functions/functions.js
@@ -15,11 +15,38 @@ se_apresentar("Maria", 30);
 se_apresentar("Bruno", 25);
 se_apresentar("Miguel");
 
-function criar_nome_completo(nome, sobrenome) {
-    return `${nome} ${sobrenome}`; 
+function criar_nome_completo(nome, sobrenome, sexo) {
+    sexo = sexo.toUpperCase();
+    if(sexo == "M") {
+        return `Sr. ${nome} ${sobrenome}`;
+    } else if (sexo == "F") {
+        return `Sra. ${nome} ${sobrenome}`;
+    } else {
+        return `${nome} ${sobrenome}`;
+    }
 }
 
-const nomeCompleto = criar_nome_completo("Leandro", "Clayton");
+const nomeCompleto = criar_nome_completo("Leandro", "Clayton", "m");
+const nomeCompleto2 = criar_nome_completo("Julia", "Silva", "F");
 
 dizer_ola(nomeCompleto);
-console.log(nomeCompleto);
+dizer_ola(nomeCompleto2);
+
+// Arrow Functions
+
+const dizer_oi = () => console.log("Oi");
+dizer_oi();
+
+const dizer_oi_tudo_bem = () => {
+    console.log("Oi!");
+    console.log("Tudo bem?");
+}
+dizer_oi_tudo_bem();
+
+const somar = (x, y) => {
+    return x + y;
+}
+
+const resultado = somar(10, 15);
+
+console.log(resultado);
